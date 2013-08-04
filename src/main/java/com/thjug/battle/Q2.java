7076d3237c3c73f839df14c3734cf73e8200dd3f
nuboat <nuboat@gmail.com>
2013-08-04 23:26:13 +0700

    Changed to support big input in Q2

--- src/main/java/com/thjug/battle/Q2.java
+++ src/main/java/com/thjug/battle/Q2.java
@@ -22,10 +22,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Hashtable;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
